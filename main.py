a24e64057c19f32edf2c7868d05791813af255cf
Tumbo Lobi John <johnlobiit@gmail.com>
2021-11-03 12:35:13 +0300

    get post by id

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -8,6 +11,18 @@ app = FastAPI()
 class Post(BaseModel):
     title: str
     content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "favorite foods", "content": "I like chicken", "id": 2}]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
 
 
 @app.get("/")
@@ -17,11 +32,21 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your post"}
+    return {"data": my_posts}
 
 
-@app.post("/createposts")
-def create_posts(new_post: Post):
-    print(new_post.title)
-    return {"data": "new post"}
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
 # title str, content str
+
+
+@app.get("/posts/{id}")
+def get_post(id):
+    post = find_post(int(id))
+    return {"post_detail": post}

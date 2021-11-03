a67d7594be20b2b306676b3392dca498db178bb9
Tumbo Lobi John <johnlobiit@gmail.com>
2021-11-03 11:44:29 +0300

    created a post request

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
 
 
 @app.get("/")
@@ -11,3 +18,10 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your post"}
+
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    print(new_post.title)
+    return {"data": "new post"}
+# title str, content str

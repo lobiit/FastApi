a10ff4206a7bb6102a1466ad5193d780a6c73949
Tumbo Lobi John <johnlobiit@gmail.com>
2021-11-04 17:58:01 +0300

    added get a specific post using id

--- app/main.py
+++ app/main.py
@@ -68,15 +68,11 @@ def create_posts(post: Post):
 
 # title str, content str
 
-@app.get("/posts/latest")
-def get_latest_post():
-    post = my_posts[len(my_posts) - 1]
-    return {"detail": post}
-
-
 @app.get("/posts/{id}")
 def get_post(id: int, response: Response):
-    post = find_post(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
+    post = cursor.fetchone()
+
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id: {id} was not found')
         # response.status_code = status.HTTP_404_NOT_FOUND

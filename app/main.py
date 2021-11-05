59d85b62f31bfcce9d410f4a00beb1a398b82928
Tumbo Lobi John <johnlobiit@gmail.com>
2021-11-05 12:12:41 +0300

    file restructure

--- app/main.py
+++ app/main.py
@@ -2,24 +2,18 @@ from typing import Optional
 
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
-from pydantic import BaseModel
+
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from . import models
+from . import models, schema
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
 
 
 while True:
@@ -70,7 +64,7 @@ def get_posts(db: Session = Depends(get_db)):
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
+def create_posts(post: schema.Post, db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
     #               (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
@@ -113,7 +107,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+def update_post(id: int, updated_post: schema.Post, db: Session = Depends(get_db)):
     # cursor.execute("""UPDATE posts  SET title= %s, content = %s, published = %s WHERE id = %s RETURNING *""",
     #              (post.title, post.content, post.published, str(id)))
 

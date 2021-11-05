f6510c70866ae5516bbdd81a8a9e2382c758983b
Tumbo Lobi John <johnlobiit@gmail.com>
2021-11-05 11:32:54 +0300

    added create posts functionality using the ORM

--- app/main.py
+++ app/main.py
@@ -57,9 +57,8 @@ def root():
 
 @app.get("/sqlalchemy")
 def test_posts(db: Session = Depends(get_db)):
-    
     posts = db.query(models.Post).all()
-    return{'status': posts}
+    return {'status': posts}
 
 
 @app.get("/posts")
@@ -71,11 +70,16 @@ def get_posts(db: Session = Depends(get_db)):
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
-                   (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_posts(post: Post, db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
+    #               (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"data": new_post}
 
 

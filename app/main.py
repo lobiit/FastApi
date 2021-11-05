f9e739fad23b0d409b72d09cb6df8182dfaafa4e
Tumbo Lobi John <johnlobiit@gmail.com>
2021-11-05 10:13:25 +0300

    modified the database model

--- app/main.py
+++ app/main.py
@@ -1,26 +1,19 @@
 from typing import Optional
 
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from sqlalchemy.orm import Session
 from . import models
-from .database import engine, SessionLocal
+from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 class Post(BaseModel):
@@ -60,6 +53,11 @@ def find_index_post(id):
 @app.get("/")
 def root():
     return {"message": "Hello John"}
+
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    return{'status': 'success'}
 
 
 @app.get("/posts")

f76fb475a93c67e60bab13604257d2f25a4d8f59
Tumbo Lobi John <johnlobiit@gmail.com>
2021-11-10 09:27:18 +0300

    added search functionality

--- app/routers/post.py
+++ app/routers/post.py
@@ -16,10 +16,10 @@ router = APIRouter(
 
 @router.get("/", response_model=List[schema.Post])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),
-              limit: int = 10, skip: int = 0):
+              limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cursor.execute(""" SELECT * FROM posts """)
     # posts = cursor.fetchall()
-    posts = db.query(models.Post).limit(limit).offset(skip).all()
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 

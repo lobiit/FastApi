51d7981f90e83bea4ccc9cee2acb54903e57509f
Tumbo Lobi John <johnlobiit@gmail.com>
2021-11-10 11:17:19 +0300

    clean up: added a .env file instead of hard coding figures

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,18 +1,20 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from . import schema, database, models
-from sqlalchemy.orm import Session
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from .config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
-# SECRET KEY
-# ALGORITHM
-# EXPIRATION TIME
 
-SECRET_KEY = "ASDSAdjJAHSBHDBBXSDBCHJBB4U5T75U49E8UI357YOGURT6TGH5T46"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+# SECRET_KEY
+# Algorithm
+# Expriation time
+
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 
 def create_access_token(data: dict):
@@ -27,24 +29,26 @@ def create_access_token(data: dict):
 
 
 def verify_access_token(token: str, credentials_exception):
+
     try:
-        print(token)
+
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         id: str = payload.get("user_id")
-
         if id is None:
             raise credentials_exception
-
         token_data = schema.TokenData(id=id)
-    except JWTError as e:
+    except JWTError:
         raise credentials_exception
+
     return token_data
 
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db())):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                          detail=f"Could not validate credentials",
-                                          headers={"WWW-Authenticate": "Bearer"})
+                                          detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+
     token = verify_access_token(token, credentials_exception)
+
     user = db.query(models.User).filter(models.User.id == token.id).first()
+
     return user
